2fe8b90880caac078197811948dc7d226e137da0
To Minh Cuong <tominhcuong5g@gmail.com>
2026-08-27 10:20:46 +0700

    Test rectangle caculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
 import {calculateCircle} from './shapes/circle/circleCalculator';
+import {calculateRectangle} from './shapes/rectangle/rectangleCalculator';
 function App(){
   const [radius, setRadius] = useState(6.4)
   const result = calculateCircle(radius);
+
+  const[lenght, setLength] = useState(5);
+  const[width, setWidth] = useState(12);
+  const result2 = calculateRectangle(lenght, width);
   return (
     <div>
       <h1>Circle Calculator</h1>
       <p>Diameter: {result.diameter}</p>
       <p>Circumference: {result.circumference}</p>
       <p>Area: {result.area}</p>
+
+      <h1>Rectangle Calculator</h1>
+      <p>Perimeter: {result2.perimeter}</p>
+      <p>Area: {result2.area}</p>
     </div>
   );
 }
