d952b15d94d6f51a848c04f67cacfd13932de6c2
To Minh Cuong <tominhcuong5g@gmail.com>
2026-08-27 10:21:56 +0700

    Test triangle calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {calculateCircle} from './shapes/circle/circleCalculator';
 import {calculateRectangle} from './shapes/rectangle/rectangleCalculator';
+import {calculateTriangle} from './shapes/triangle/triangleCalculator';
 function App(){
   const [radius, setRadius] = useState(6.4)
   const result = calculateCircle(radius);
@@ -8,6 +9,10 @@ function App(){
   const[lenght, setLength] = useState(5);
   const[width, setWidth] = useState(12);
   const result2 = calculateRectangle(lenght, width);
+
+  const[base, setBase] = useState(8);
+  const[height, setHeight] = useState(10);
+  const result3 = calculateTriangle(base, height);
   return (
     <div>
       <h1>Circle Calculator</h1>
@@ -18,6 +23,9 @@ function App(){
       <h1>Rectangle Calculator</h1>
       <p>Perimeter: {result2.perimeter}</p>
       <p>Area: {result2.area}</p>
+
+      <h1>Triangle Calculator</h1>
+      <p>Area: {result3.area}</p>
     </div>
   );
 }
