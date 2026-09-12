2caa564fdc4a1f6e1cf9d10c3473c8a845d30415
To Minh Cuong <tominhcuong5g@gmail.com>
2026-09-12 21:52:21 +0700

    Add deleteHistory in Routes

--- backend/src/routes/historyRoutes.js
+++ backend/src/routes/historyRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import {createHistory, getHistory} from '../controllers/historyController.js';
+import {createHistory, getHistory, deleteHistory} from '../controllers/historyController.js';
 
 const router = express.Router();
 
 router.post("/", createHistory);
 router.get("/", getHistory);
+router.delete("/", deleteHistory);
 
 export default router;
