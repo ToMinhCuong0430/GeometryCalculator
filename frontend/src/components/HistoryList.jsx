fde512dcd655357a845ea30120a6cdfcf3aafda0
To Minh Cuong <tominhcuong5g@gmail.com>
2026-09-13 10:51:38 +0700

    fix bug in code stripplet

--- frontend/src/components/HistoryList.jsx
+++ frontend/src/components/HistoryList.jsx
@@ -3,30 +3,31 @@ import React from 'react';
 function HistoryList({ history }) {
   return (
     <>
-        <h2>Calculation History</h2>
-        {history.length === 0 ? (
-          <p>No calculations yet.</p>
-        ) : (   
-         <ul>
-            {history.map((item) => (
-                <li key={item._id}>
-                    <strong>{item.shape}</strong>
-                    <div>
-                        {Object.entries(item.inputs).map(([key, value]) => (
-                            <p key={key}>{key}: {value}</p>
-                        ))}        
-                    </div>
-                    <div>
-                        {Object.entries(item.result).map(([key, value]) => (
-                            <p key={key}>{key}: {value}</p>
-                        ))}
-                    </div>
-                </li>
-            ))}        
-         </ul>
-        )}
+      <h2>Calculation History</h2>
+      
+      {history.length === 0 ? (
+        <p>No calculations yet.</p>
+      ) : (   
+        <ul>
+          {history.map((item) => (
+            <li key={item._id}>
+              <strong>{item.shape}</strong>
+              <div>
+                {Object.entries(item.inputs || {}).map(([key, value]) => (
+                  <p key={key}>{`${key}: ${value}`}</p>
+                ))}        
+              </div>
+              <div>
+                {Object.entries(item.result || {}).map(([key, value]) => (
+                  <p key={key}>{`${key}: ${value}`}</p>
+                ))}
+              </div>
+            </li>
+          ))}        
+        </ul>
+      )}
     </>
-    );
+  );
 }
 
 export default HistoryList;
